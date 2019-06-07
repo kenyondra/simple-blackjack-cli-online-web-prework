45251b54695ba99413296210b08b26e3cc2038ac
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-07 03:37:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,7 +64,8 @@ def runner
     card_total = hit? card_total
     card_total = display_card_total(card_total)
   end
-    end_game 
+    end_game(card_total) 
+  end
   # code runner here
 end
     
